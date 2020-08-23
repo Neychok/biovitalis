e3e30ee7217442efe6ec88ad50750de1cb911795
Neycho <n.kalaidjiev@gmail.com>
2020-08-23 12:55:46 +0300

    adding Prismic.io to project. Closes #6

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,112 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: "neycho",
+        accessToken:
+          "MC5YMEZMMkJBQUFDUUFCSURl.Ehbvv73vv73vv71gEO-_ve-_vRLvv70H77-9Ie-_vUEl77-977-9EAdXQxnvv71Z77-9MBBzFAo",
+        linkResolver: ({ node, key, value }) => doc => {
+          if (doc.type === "product") return "/product/" + doc.uid
+          if (doc.type === "page") return "/" + doc.uid
+          // Fallback for other types, in case new custom types get created
+          return "/doc/" + doc.id
+        },
+
+        // Set a list of links to fetch and be made available in your link
+        // resolver function.
+        // See: https://prismic.io/docs/javascript/query-the-api/fetch-linked-document-fields
+        fetchLinks: [
+          // Your list of links
+        ],
+
+        // Set an HTML serializer function used to process formatted content.
+        // Fields with rich text formatting use this function to generate the
+        // correct HTML.
+        // The document node, field key (i.e. API ID), and field value are
+        // provided to the function, as seen below. This allows you to use
+        // different HTML serializer logic for each field if necessary.
+        // See: https://prismic.io/docs/nodejs/beyond-the-api/html-serializer
+        htmlSerializer: ({ node, key, value }) => (
+          type,
+          element,
+          content,
+          children
+        ) => {
+          // switch(type) {
+          //   // Add a class to paragraph elements
+          //   case Elements.paragraph:
+          //     return '<p class="paragraph-class">' + children.join('') + '</p>';
+          //   // Don't wrap images in a <p> tag
+          //   case Elements.image:
+          //     return '<img src="' + element.url + '" alt="' + element.alt + '">';
+          //   // Add a class to hyperlinks
+          //   case Elements.hyperlink:
+          //     var target = element.data.target ? 'target="' + element.data.target + '" rel="noopener"' : '';
+          //     var linkUrl = PrismicDOM.Link.url(element.data, linkResolver);
+          //     return '<a class="some-link"' + target + ' href="' + linkUrl + '">' + content + '</a>';
+          //   // Return null to stick with the default behavior for all other elements
+          //   default:
+          //     return null;
+          // }
+        },
+
+        // Provide an object of Prismic custom type JSON schemas to load into
+        // Gatsby. This is required.
+        schemas: {
+          // Your custom types mapped to schemas
+          product: require("./src/schemas/product.json"),
+        },
+
+        // Set a default language when fetching documents. The default value is
+        // '*' which will fetch all languages.
+        // See: https://prismic.io/docs/javascript/query-the-api/query-by-language
+        lang: "*",
+
+        // Add the Prismic Toolbar script to the site. Defaults to false.
+        // Set to "legacy" if your repository requires the older toolbar script.
+        // See: https://prismic.io/docs/rest-api/beyond-the-api/the-preview-feature
+        prismicToolbar: true,
+
+        // Set a function to determine if images are downloaded locally and made
+        // available for gatsby-transformer-sharp for use with gatsby-image.
+        // The document node, field key (i.e. API ID), and field value are
+        // provided to the function, as seen below. This allows you to use
+        // different logic for each field if necessary.
+        // This defaults to always return false.
+        shouldDownloadImage: ({ node, key, value }) => {
+          // Return true to download the image or false to skip.
+        },
+
+        // Provide a default set of Imgix image transformations applied to
+        // Imgix-backed gatsby-image fields. These options will override the
+        // defaults set by Prismic.
+        // See: https://docs.imgix.com/apis/url
+        imageImgixParams: {
+          auto: "compress,format",
+          fit: "max",
+          q: 50,
+        },
+
+        // Provide a default set of Imgix image transformations applied to
+        // the placeholder images of Imgix-backed gatsby-image fields. These
+        // parameters will be applied over those provided in the above
+        // `imageImgixParams` option.
+        // See: https://docs.imgix.com/apis/url
+        imagePlaceholderImgixParams: {
+          w: 100,
+          blur: 15,
+          q: 50,
+        },
+
+        // Set the prefix for the filename where type paths for your schemas are
+        // stored. The filename will include the MD5 hash of your schemas after
+        // the prefix.
+        // This defaults to 'prismic-typepaths---${repositoryName}'.
+        typePathsFilenamePrefix: "prismic-typepaths---neycho",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
