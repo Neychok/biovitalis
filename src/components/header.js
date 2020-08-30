b6bda8677e1ff267a632a8304dc58fd0bd57e55c
Neycho <n.kalaidjiev@gmail.com>
2020-08-30 20:19:50 +0300

    Removed sticky

--- src/components/header.js
+++ src/components/header.js
@@ -46,7 +46,7 @@ function Header({ siteTitle }) {
       {/* Menu Contents */}
       <div
         className={`${
-          isExpanded ? `block` : `hidden`
+          isExpanded ? `block sticky` : `hidden`
         } w-full bg-teal-500 flex-grow lg:flex lg:items-center lg:w-auto`}
       >
         <div className="text-sm lg:flex-grow">
@@ -87,20 +87,6 @@ function Header({ siteTitle }) {
     </nav>
   )
 }
-
-var nav = document.getElementById("site-menu")
-var header = document.getElementById("top")
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 400) {
-    // adjust this value based on site structure and header image height
-    nav.classList.add("nav-sticky")
-    header.classList.add("pt-scroll")
-  } else {
-    nav.classList.remove("nav-sticky")
-    header.classList.remove("pt-scroll")
-  }
-})
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
