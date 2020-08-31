2d33d8c34afbe2e24536a99cc70afa5d4cd2d860
Neycho <n.kalaidjiev@gmail.com>
2020-09-01 01:27:22 +0300

    Bug fix / Gatsby window error /

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,22 @@
 import PropTypes from "prop-types"
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, ComponentDidMount } from "react"
 import SideBar from "./sidebar"
 import NavigationBar from "./navigationbar"
 
 const Header = () => {
-  const [isDesktop, setDesktop] = useState(window.innerWidth > 768)
+  if (typeof window !== `undefined`) {
+    const [isDesktop, setDesktop] = useState(window.innerWidth > 768)
+    const updateMedia = () => {
+      setDesktop(window.innerWidth > 768)
+    }
 
-  const updateMedia = () => {
-    setDesktop(window.innerWidth > 768)
-  }
+    useEffect(() => {
+      window.addEventListener("resize", updateMedia)
+      return () => window.removeEventListener("resize", updateMedia)
+    })
 
-  useEffect(() => {
-    window.addEventListener("resize", updateMedia)
-    return () => window.removeEventListener("resize", updateMedia)
-  })
-
-  return <div>{isDesktop ? <NavigationBar /> : <SideBar />}</div>
+    return <div>{isDesktop ? <NavigationBar /> : <SideBar />}</div>
+  } else return null
 }
 
 Header.propTypes = {
