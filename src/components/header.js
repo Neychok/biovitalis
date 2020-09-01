222ffe04818ce557bf9b4797fca8e1e27e4dbe66
Neycho <n.kalaidjiev@gmail.com>
2020-09-01 14:25:08 +0300

    actual fix to gatsby not rendering window

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,19 @@
 import PropTypes from "prop-types"
-import React, { useState, useEffect, ComponentDidMount } from "react"
+import React from "react"
 import SideBar from "./sidebar"
 import NavigationBar from "./navigationbar"
 
 const Header = () => {
-  if (typeof window !== `undefined`) {
-    const [isDesktop, setDesktop] = useState(window.innerWidth > 768)
-    const updateMedia = () => {
-      setDesktop(window.innerWidth > 768)
-    }
-
-    useEffect(() => {
-      window.addEventListener("resize", updateMedia)
-      return () => window.removeEventListener("resize", updateMedia)
-    })
-
-    return <div>{isDesktop ? <NavigationBar /> : <SideBar />}</div>
-  } else return null
+  return (
+    <>
+      <div className="visible md:invisible">
+        <SideBar />
+      </div>
+      <div className="invisible md:visible">
+        <NavigationBar />
+      </div>
+    </>
+  )
 }
 
 Header.propTypes = {
