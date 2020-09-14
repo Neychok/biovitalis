51146d387a26e5526933c0db508164e3295099b1
Neycho <n.kalaidjiev@gmail.com>
2020-09-15 01:24:18 +0300

    removed service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -138,7 +138,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,  
+        display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
@@ -258,5 +258,6 @@ module.exports = {
 
     // Plugin that adds offline support
     `gatsby-plugin-offline`,
+    "gatsby-plugin-remove-serviceworker",
   ],
 }
