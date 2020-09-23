c4946530407d2550fa49f0d24d2a0fd92689a789
Neycho <n.kalaidjiev@gmail.com>
2020-09-23 22:10:46 +0300

    added lightbox

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -4,28 +4,57 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/swiper-bundle.min.css"
 import "lazysizes"
 import "lazysizes/plugins/parent-fit/ls.parent-fit"
+import SimpleReactLightbox from "simple-react-lightbox"
+import { SRLWrapper } from "simple-react-lightbox"
 
 SwiperCore.use([Thumbs])
+
+const options = {
+  settings: {
+    overlayColor: "#000000E6",
+    autoplaySpeed: 0,
+    slideAnimationType: "fade",
+    disableWheelControls: true,
+  },
+  buttons: {
+    showDownloadButton: false,
+    showThumbnailsButton: false,
+    size: "50px",
+    showNextButton: true,
+    showPrevButton: true,
+  },
+  caption: {
+    showCaption: false,
+  },
+  translations: {
+    closeText: "Затвори",
+    fullscreenText: "Цял Екран",
+    nextText: "Напред",
+    previousText: "Назад",
+  },
+}
 
 const Carousel = ({ images }) => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null)
   return (
     <div>
-      <Swiper thumbs={{ swiper: thumbsSwiper }} className="">
-        {images.map(slide => (
-          <SwiperSlide key={slide.src}>
-            <img
-              src={`${slide.image.fluid.src}`}
-              srcSet={`${slide.image.fluid.base64}`}
-              data-srcset={`${slide.image.fluid.srcSet}`}
-              data-sizes="auto"
-              className="lazyload block w-full"
-              alt={`${slide.image.url}`}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
+      <SimpleReactLightbox>
+        <SRLWrapper options={options}>
+          <Swiper thumbs={{ swiper: thumbsSwiper }} className="">
+            {images.map(slide => (
+              <SwiperSlide key={slide.src}>
+                <img
+                  src={`${slide.image.fluid.src}`}
+                  data-srcset={`${slide.image.fluid.srcSet}`}
+                  data-sizes="auto"
+                  className="lazyload block w-full"
+                  alt={`${slide.image.url}`}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </SRLWrapper>
+      </SimpleReactLightbox>
       <Swiper onSwiper={setThumbsSwiper} spaceBetween={2} slidesPerView={3}>
         {images.map(slide => (
           <SwiperSlide key={slide.src} className="">
