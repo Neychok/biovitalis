205d18ebf50aa857b12c1a895aedaba7460f7c5a
Neycho Kalaydzhiev <n.kalaidjiev@gmail.com>
2021-03-03 01:24:23 +0200

    forgot this

--- packages/studio/schemas/objects/juicePressingProductsTabs.js
+++ packages/studio/schemas/objects/juicePressingProductsTabs.js
@@ -75,7 +75,8 @@ export default {
         source: "name",
         maxLength: 200,
         slugify: input =>
-          input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
+          input.toLowerCase().charAt(0).toUpperCase() +
+          input.slice(1).replace(/\s+/g, "-").slice(0, 200),
       },
     },
 
